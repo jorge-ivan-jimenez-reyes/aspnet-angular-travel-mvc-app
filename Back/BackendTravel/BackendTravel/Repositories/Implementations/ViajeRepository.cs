f6dc3aac1e0439a5f226d3925c98fd9aa63d7044
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2025-03-23 15:47:37 -0600

    viajes

--- Back/BackendTravel/BackendTravel/Repositories/Implementations/ViajeRepository.cs
+++ Back/BackendTravel/BackendTravel/Repositories/Implementations/ViajeRepository.cs
@@ -1,6 +1,18 @@
 namespace BackendTravel.Repositories.Implementations;
 
-public class ViajeRepository
+public class ViajeRepository : IViajeRepository
 {
-    
+    private readonly ApplicationDbContext _context;
+
+    public ViajeRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Viaje>> GetAllAsync()
+    {
+        return await _context.Viajes.ToListAsync();
+    }
+
+    // ... Resto de métodos
 }
