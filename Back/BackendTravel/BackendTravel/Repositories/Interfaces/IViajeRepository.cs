3f37ee235ea75bdc60205caa9352a44219f9cc0f
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2025-03-23 15:47:01 -0600

    repo

--- Back/BackendTravel/BackendTravel/Repositories/Interfaces/IViajeRepository.cs
+++ Back/BackendTravel/BackendTravel/Repositories/Interfaces/IViajeRepository.cs
@@ -1,6 +1,10 @@
-namespace BackendTravel.Repositories.Implementations;
+namespace BackendTravel.Repositories.Interfaces;
 
 public interface IViajeRepository
 {
-    
+    Task<IEnumerable<Viaje>> GetAllAsync();
+    Task<Viaje> GetByIdAsync(int id);
+    Task AddAsync(Viaje viaje);
+    void Update(Viaje viaje);
+    void Delete(Viaje viaje);
 }
