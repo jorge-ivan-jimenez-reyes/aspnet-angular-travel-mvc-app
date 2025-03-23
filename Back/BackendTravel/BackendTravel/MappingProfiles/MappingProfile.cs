7ecedc9c9fcc33f18afbee124f0b4ac37cf474cd
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2025-03-23 16:00:41 -0600

    d

--- Back/BackendTravel/BackendTravel/MappingProfiles/MappingProfile.cs
+++ Back/BackendTravel/BackendTravel/MappingProfiles/MappingProfile.cs
@@ -1,6 +1,16 @@
-namespace BackendTravel.MappingProfiles;
 
-public class MappingProfile
+using AutoMapper;
+using BackendTravel.Models.Entities;
+using BackendTravel.Models.DTOs;
+
+namespace BackendTravel.MappingProfiles
 {
-    
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Mapeo bidireccional entre Viaje y ViajeDto
+            CreateMap<Viaje, ViajeDto>().ReverseMap();
+        }
+    }
 }
