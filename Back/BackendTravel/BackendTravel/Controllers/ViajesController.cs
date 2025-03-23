caaaa5b0d46aeb865834bc4f19f0bbedd0fb134e
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2025-03-23 15:06:42 -0600

    viajes

--- Back/BackendTravel/BackendTravel/Controllers/ViajesController.cs
+++ Back/BackendTravel/BackendTravel/Controllers/ViajesController.cs
@@ -3,10 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BackendTravel.Controllers
 {
-    //
+    //localhost:xxxx/api/viajes
     [Route("api/[controller]")]
     [ApiController]
     public class ViajesController : ControllerBase
     {
+        
     }
 }
