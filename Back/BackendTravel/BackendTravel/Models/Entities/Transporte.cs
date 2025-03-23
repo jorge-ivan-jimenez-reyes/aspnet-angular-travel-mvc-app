9f3da6caed6af8d814636e7ce4cd403725bad9c5
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2025-03-23 14:46:11 -0600

    Transporte.cs

--- Back/BackendTravel/BackendTravel/Models/Entities/Transporte.cs
+++ Back/BackendTravel/BackendTravel/Models/Entities/Transporte.cs
@@ -2,5 +2,10 @@ namespace BackendTravel.Models.Entities;
 
 public class Transporte
 {
-    
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string? Tipo { get; set; } // Ej. Avión, Bus
+    public bool Activo { get; set; } = true;
+
+    public ICollection<Viaje> Viajes { get; set; }
 }
