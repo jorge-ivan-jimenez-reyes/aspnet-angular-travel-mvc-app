d76b7c16150d72f63a87e4d4c3c432d286f27b8f
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2025-03-23 13:48:19 -0600

    db context

--- Back/BackendTravel/BackendTravel/Data/ApplicationDBContext.cs
+++ Back/BackendTravel/BackendTravel/Data/ApplicationDBContext.cs
@@ -2,11 +2,28 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BackendTravel.Data;
 
-public class ApplicationDBContext : DbContext
-
+public class AppDbContext : DbContext
 {
-     public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+     public DbSet<Viaje> Viajes { get; set; }
+     public DbSet<Lugar> Lugares { get; set; }
+     public DbSet<Transporte> Transportes { get; set; }
+     public DbSet<EstatusViaje> EstatusesViaje { get; set; }
+
+     protected override void OnModelCreating(ModelBuilder modelBuilder)
      {
-          
+          // Relaciones personalizadas para Origen/Destino
+          modelBuilder.Entity<Viaje>()
+               .HasOne(v => v.Origen)
+               .WithMany(l => l.ViajesOrigen)
+               .HasForeignKey(v => v.OrigenId)
+               .OnDelete(DeleteBehavior.Restrict);
+
+          modelBuilder.Entity<Viaje>()
+               .HasOne(v => v.Destino)
+               .WithMany(l => l.ViajesDestino)
+               .HasForeignKey(v => v.DestinoId)
+               .OnDelete(DeleteBehavior.Restrict);
      }
 }
