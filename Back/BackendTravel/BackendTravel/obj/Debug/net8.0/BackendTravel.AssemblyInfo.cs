f9238d35aa912559d310ed17b92b692f8e140996
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2025-03-23 16:30:06 -0600

    fixing bugs

--- Back/BackendTravel/BackendTravel/obj/Debug/net8.0/BackendTravel.AssemblyInfo.cs
+++ Back/BackendTravel/BackendTravel/obj/Debug/net8.0/BackendTravel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendTravel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+174342104a07146fcaee0a6f656e61e341a28b19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34200662de3a6b381c9d653972545bf09445129b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendTravel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendTravel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
