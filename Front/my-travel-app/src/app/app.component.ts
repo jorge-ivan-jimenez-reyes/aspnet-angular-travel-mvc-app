f0e60d68d24cf5a60ca6cfeac28fda1204f9cc90
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2025-03-23 20:18:44 -0600

    Remove ViajesComponent integration and update app component structure for routing

--- Front/my-travel-app/src/app/app.component.ts
+++ Front/my-travel-app/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ViajesComponent } from './viajes/componentes/viajes.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ViajesComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
