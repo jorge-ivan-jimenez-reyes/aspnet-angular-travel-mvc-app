fc2bcff0e2dd800552dda58c36e1d5dcc5732146
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2025-03-23 20:34:18 -0600

    Replace ViajesComponent with CommonModule in app component imports for improved modularity

--- Front/my-travel-app/src/app/app.component.ts
+++ Front/my-travel-app/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ViajesComponent } from './viajes/componentes/viajes.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ViajesComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'my-travel-app';
--- Front/my-travel-app/src/app/app.config.ts
+++ Front/my-travel-app/src/app/app.config.ts
@@ -1,13 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideClientHydration } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    importProvidersFrom(CommonModule, RouterModule),
     provideRouter(routes),
     provideHttpClient(),
     provideAnimations(),
