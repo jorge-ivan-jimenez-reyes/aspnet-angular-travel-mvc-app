2a88dc395156c9c42ac5dae8ba655047283f8a96
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2025-03-24 12:06:19 -0600

    Update workspace configuration and assembly info for improved build output and logging

--- Back/BackendTravel/BackendTravel/obj/Debug/net8.0/BackendTravel.AssemblyInfo.cs
+++ Back/BackendTravel/BackendTravel/obj/Debug/net8.0/BackendTravel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendTravel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+678483746778b5a17917a092d7865b610279f192")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b71574dab500335c9e53f510d42bbdefb88fcb34")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendTravel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendTravel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
