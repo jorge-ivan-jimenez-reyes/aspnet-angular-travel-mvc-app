2592b41685e1d9e6ef78cf11edf57f80e74b5e48
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2025-03-23 22:59:45 -0600

    Add ViajesComponent with initial template, styles, and unit tests

--- Front/viajes/src/app/app.routes.ts
+++ Front/viajes/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { ViajesComponent } from './viajes/components/viajes/viajes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'viajes',
+        pathMatch: 'full'
+
+
+
+
+
+    },
+    {
+        path: 'viajes',
+        component: ViajesComponent
+    }
+    
+];
