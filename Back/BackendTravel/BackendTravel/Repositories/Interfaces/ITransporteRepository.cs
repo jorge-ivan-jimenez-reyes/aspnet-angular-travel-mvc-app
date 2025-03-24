ff9060b7d9b7e59d1ecafa95ba13bd3ad2347000
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2025-03-23 18:23:20 -0600

    Add GetAllAsync method to ITransporteRepository interface

--- Back/BackendTravel/BackendTravel/Repositories/Interfaces/ITransporteRepository.cs
+++ Back/BackendTravel/BackendTravel/Repositories/Interfaces/ITransporteRepository.cs
@@ -1,6 +1,12 @@
+
+
+using BackendTravel.Models.Entities;
+
+
 namespace BackendTravel.Repositories.Interfaces;
 using BackendTravel.Models.Entities;
 public interface ITransporteRepository
 {
+    Task<IEnumerable<Transporte>> GetAllAsync();
     
 }
