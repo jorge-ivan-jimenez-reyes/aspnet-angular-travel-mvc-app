a8f75a33e4f1bb859b9ae7f135cd245ba9ead454
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2025-03-23 18:26:16 -0600

    Add GetAllAsync method to IEstatusViajeRepository interface

--- Back/BackendTravel/BackendTravel/Repositories/Interfaces/IEstatusViajeRepository.cs
+++ Back/BackendTravel/BackendTravel/Repositories/Interfaces/IEstatusViajeRepository.cs
@@ -1,6 +1,9 @@
+
+using BackendTravel.Models.Entities;
+
 namespace BackendTravel.Repositories.Interfaces;
 
 public interface IEstatusViajeRepository
 {
-    
+    Task<IEnumerable<EstatusViaje>> GetAllAsync();
 }
