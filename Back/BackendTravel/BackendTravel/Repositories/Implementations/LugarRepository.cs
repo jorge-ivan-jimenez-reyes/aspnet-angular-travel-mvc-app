19b31b5ab670eefe1a2fa3d3a2ec389b2d058909
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2025-03-23 18:09:40 -0600

    b

--- Back/BackendTravel/BackendTravel/Repositories/Implementations/LugarRepository.cs
+++ Back/BackendTravel/BackendTravel/Repositories/Implementations/LugarRepository.cs
@@ -1,6 +1,23 @@
-namespace BackendTravel.Repositories.Implementations;
+using BackendTravel.Data;
+using BackendTravel.Models.Entities;
+using BackendTravel.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
-public class LugarRepository
+
+namespace BackendTravel.Repositories.Implementations
 {
-    
+    public class LugarRepository : ILugarRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LugarRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Lugar>> GetAllAsync()
+        {
+            return await _context.Lugares.ToListAsync();
+        }
+    }
 }
