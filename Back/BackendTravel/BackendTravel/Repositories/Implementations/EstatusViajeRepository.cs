ace3faa6835918ef51b4b5790058ec44502f191c
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2025-03-23 18:27:28 -0600

    Implement GetAllAsync method in EstatusViajeRepository

--- Back/BackendTravel/BackendTravel/Repositories/Implementations/EstatusViajeRepository.cs
+++ Back/BackendTravel/BackendTravel/Repositories/Implementations/EstatusViajeRepository.cs
@@ -1,6 +1,22 @@
+using BackendTravel.Data;
+using BackendTravel.Models.Entities;
+using BackendTravel.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+
 namespace BackendTravel.Repositories.Implementations;
 
 public class EstatusViajeRepository
 {
-    
+    private readonly AppDbContext _context;
+
+    public EstatusViajeRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<EstatusViaje>> GetAllAsync()
+    {
+        return await _context.EstatusesViaje.ToListAsync();
+    }
 }
