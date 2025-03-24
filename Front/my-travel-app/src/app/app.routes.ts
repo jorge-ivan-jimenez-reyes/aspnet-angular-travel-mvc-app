7d795d67d22dcc72bc7bf470770cf70a6c853fb1
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2025-03-23 19:24:58 -0600

    Update workspace configuration to enhance DbContext with sensitive data logging and console output

--- Front/my-travel-app/src/app/app.routes.ts
+++ Front/my-travel-app/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./viajes/componentes/viajes.component').then(m => m.ViajesComponent)
+  }
+];
