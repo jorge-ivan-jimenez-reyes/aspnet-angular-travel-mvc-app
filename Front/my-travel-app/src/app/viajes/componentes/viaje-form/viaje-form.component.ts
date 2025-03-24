75fddda9ea1fb80137fb75a629ca831b35db5e38
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2025-03-23 20:24:04 -0600

    Add ViajeFormComponent with initial template, styles, and tests

---
+++ Front/my-travel-app/src/app/viajes/componentes/viaje-form/viaje-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-viaje-form',
+  imports: [],
+  templateUrl: './viaje-form.component.html',
+  styleUrl: './viaje-form.component.scss'
+})
+export class ViajeFormComponent {
+
+}
